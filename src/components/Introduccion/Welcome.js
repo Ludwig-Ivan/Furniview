343cc2f14d432cc96d22b3b3da41a20e9c23d645
Ludwig Ivan <ivansierras11@gmail.com>
2024-04-24 21:30:27 -0600

    Animation Welcome

--- src/components/Introduccion/Welcome.js
+++ src/components/Introduccion/Welcome.js
@@ -1,11 +1,42 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useEffect} from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import Background from '../common/Background';
 import {Logos} from '../../constants';
+import Animated, {
+  Easing,
+  useSharedValue,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
 
 const Welcome = ({navigation}) => {
+  const fontSize = useSharedValue(32);
+  const width = useSharedValue(300);
+  const height = useSharedValue(460);
+
   useEffect(() => {
+    fontSize.value = withDelay(
+      1500,
+      withTiming(fontSize.value - 32, {
+        duration: 5000,
+        easing: Easing.ease,
+      }),
+    );
+    width.value = withDelay(
+      5000,
+      withTiming(width.value - 300, {
+        duration: 5000,
+        easing: Easing.sin,
+      }),
+    );
+    height.value = withDelay(
+      5000,
+      withTiming(height.value - 460, {
+        duration: 5000,
+        easing: Easing.sin,
+      }),
+    );
     const timer = setTimeout(() => {
       navigation.navigate('Home');
     }, 10000);
@@ -15,12 +46,17 @@ const Welcome = ({navigation}) => {
   return (
     <Background>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={style.main}>
+        <Animated.View style={[style.main, {width, height}]}>
           <View style={{gap: 80, alignItems: 'center'}}>
-            <Image style={style.logo} source={Logos.logo1} />
-            <Text style={style.titulo}>BIENVENIDO</Text>
+            <Animated.Image
+              style={[style.logo, {height, width}]}
+              source={Logos.logo1}
+            />
+            <Animated.Text style={[style.titulo, {fontSize}]}>
+              BIENVENIDO
+            </Animated.Text>
           </View>
-        </View>
+        </Animated.View>
       </View>
     </Background>
   );
@@ -30,20 +66,19 @@ const style = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#D4B89E',
-    width: 300,
-    height: 460,
+    maxWidth: 300,
+    maxHeight: 460,
     borderRadius: 10,
   },
 
   logo: {
-    width: 150,
-    height: 150,
+    maxWidth: 150,
+    maxHeight: 150,
     borderRadius: 100,
   },
 
   titulo: {
     fontStyle: 'italic',
-    fontSize: 32,
     fontWeight: '900',
     color: '#382424',
   },
